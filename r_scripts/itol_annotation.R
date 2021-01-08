aab9243fa73515ab32e61bfbabdaac33a3e71608
Gary Napier <garynapier@Garys-MacBook-Pro-2.local>
2021-01-08 17:30:23 +0000

    separated template generation to itol_templates.R and started DR

--- r_scripts/itol_annotation.R
+++ r_scripts/itol_annotation.R
@@ -8,13 +8,12 @@
 # - Lineage - output of tbprofiler
 
 # Arguments to script:
-# Study accession, DR metadata file name (full path), clusters file location (dir only), lineage file name (full path), output_location
+# Study accession, itol template files location, DR metadata file name (full path), clusters file location (dir only), lineage file name (full path), output_location
 
 # Input:
-# Scraped annotation template from itol, DR metadata file, clusters file, lineage file
+# itol template files, DR metadata file, clusters file, lineage file
 
 # Main steps:
-# Scrape the annotation template from itol and clean
 # Filter DR data by study accession, clean and parse into dataframe ready for itol template
 # Get cluster membership of samples and onvert to colour scheme - parse into dataframe ready for itol template
 # Repeat for lineage
@@ -23,10 +22,20 @@
 # Three itol files, one for each type of annotation
 
 # RUN:
-# Rscript r_scripts/itol_annotation.R <study_accession> <drug_resistance_data_file> <clusters_data_file_dir> <lineage_file> <output_location>
-# Rscript r_scripts/itol_annotation.R PRJEB7669 ~/metadata/tb_data_collated_28_10_2020_clean.csv metadata/ ~/metadata/lineages_napier_barcode.txt itol_annotations
+# Rscript r_scripts/itol_annotation.R <study_accession> <metadata_file> <clusters_data_file_dir> <itol_location>
+# Rscript r_scripts/itol_annotation.R PRJEB7669 ~/metadata/tb_data_collated_28_10_2020_clean.csv metadata/ itol_annotations
 
 # Setup ----
+
+library(rvest)
+
+heaD <- function(x,...){
+  head(x, ...)
+}
+
+len_str <- function(string){
+  length(unlist(strsplit(string, split = "")))
+}
 
 # Read in args
 args <- commandArgs(trailingOnly=TRUE)
@@ -37,17 +46,21 @@ nl <- cat("\n")
 
 # Directories
 clusters_data_file_dir <- args[3]
-output_location <- args[5]
+itol_location <- args[5]
 tmp_dir <- "tmp/"
 
 # Files and suffixes/prefixes
-drug_resistance_data_file <- args[2]
-lineage_file <- args[4]
-itol_template_file <- paste0(tmp_dir, "itol_template.txt")
+metadata_file <- args[2]
+
 clusters_data_file <- paste0(clusters_data_file_dir, study_accession, ".clusters")
-itol_dr_file <- paste0(output_location, study_accession, ".dr.txt")
-itol_clusters_file <- paste0(output_location, study_accession, ".clusters.txt")
-itol_lineage_file <- paste0(output_location, study_accession, "lineages.txt")
+
+itol_dr_in_file <- paste0(itol_location, study_accession, ".dr.txt")
+itol_clusters_in_file <- paste0(itol_location, "itol.clusters.txt")
+itol_lineage_in_file <- paste0(itol_location, study_accession, "itol.lineages.txt")
+
+itol_dr_out_file <- paste0(itol_location, study_accession, "itol.dr.txt")
+itol_clusters_out_file <- paste0(itol_location, study_accession, ".clusters.txt")
+itol_lineage_out_file <- paste0(itol_location, study_accession, "lineages.txt")
 
 nl
 print("ARGUMENTS:")
@@ -56,60 +69,50 @@ print(c("Study accession:", study_accession))
 print(c("clusters_data_file_dir:", clusters_data_file_dir))
 print(c("output_location:", output_location))
 print(c("tmp_dir:", tmp_dir))
-print(c("drug_resistance_data_file:", drug_resistance_data_file))
+print(c("metadata_file:", metadata_file))
 print(c("lineage_file:", lineage_file))
-print(c("itol_template_file:", itol_template_file))
 print(c("clusters_data_file:", clusters_data_file))
 print(c("itol_dr_file:", itol_dr_file))
 print(c("itol_clusters_file:", itol_clusters_file))
 print(c("itol_lineage_file:", itol_lineage_file))
 
 
-# Scrape binary template from itol and clean
-
-# Scrape raw
-binary_template_url <- "https://itol.embl.de/help/dataset_binary_template.txt"
-binary_template_text <- html_text(read_html(binary_template_url))
-
-# Uncomment SEPARATOR TAB line and comment other two lines
-binary_template_text <- gsub("#SEPARATOR TAB", "SEPARATOR TAB", binary_template_text)
-binary_template_text <- gsub("SEPARATOR SPACE", "#SEPARATOR SPACE", binary_template_text)
-binary_template_text <- gsub("SEPARATOR COMMA", "#SEPARATOR COMMA", binary_template_text)
-
-# Replace commas with tabs
-binary_template_text <- gsub("DATASET_LABEL,label1", "DATASET_LABEL\told/new", binary_template_text)
-binary_template_text <- gsub("COLOR,#ff0000", "COLOR\t#ff0000", binary_template_text)
-binary_template_text <- gsub("FIELD_SHAPES,1", "FIELD_SHAPES\t5\t2", binary_template_text)
-binary_template_text <- gsub("FIELD_LABELS,f1", "FIELD_LABELS\told\tnew", binary_template_text)
-binary_template_text <- gsub("#FIELD_COLORS,#ff0000", "FIELD_COLORS\t#ff0000\t#000000", binary_template_text)
-# binary_template_text <- gsub("", "", binary_template_text)
-# binary_template_text <- gsub("", "", binary_template_text)
-
-
-write.table(binary_template_text, file = itol_template_file, 
-            row.names=F,col.names=F, quote = F)
 
 # Read in sample data/metadata and clean ----
 
-drug_resistance_data <- read.csv(drug_resistance_data_file, stringsAsFactors = F)
+metadata <- read.csv(metadata_file, stringsAsFactors = F)
 clusters_data <- read.table(clusters_data_file, header = T, stringsAsFactors = F)
-lineage_data <- read.delim(lineage_file, stringsAsFactors = F)
 
 # Get list of samples from clusters data
 samples <- clusters_data$id
 
-# Drug resistance clean/subset:
+# Drug resistance ----
 
-drugs <- c("rifampicin", "isoniazid", "ethambutol", "pyrazinamide", "streptomycin", "ofloxacin", 
-           "moxifloxacin", "levofloxacin", "amikacin", "kanamycin", "capreomycin", "ciprofloxacin",
-           "prothionamide", "ethionamide", "clarithromycin", "clofazimine", "bedaquiline", "cycloserine", 
-           "linezolid", "para.aminosalicylic_acid", "rifabutin", "delamanid") 
+# drugs <- c("rifampicin", "isoniazid", "ethambutol", "pyrazinamide", "streptomycin", "ofloxacin", 
+#            "moxifloxacin", "levofloxacin", "amikacin", "kanamycin", "capreomycin", "ciprofloxacin",
+#            "prothionamide", "ethionamide", "clarithromycin", "clofazimine", "bedaquiline", "cycloserine", 
+#            "linezolid", "para.aminosalicylic_acid", "rifabutin", "delamanid") 
 
-cols <- c("run_accession", "study_accession", drugs)
+cols <- c("run_accession", "study_accession", "dr_status")
 
 # Subset rows by study accession and cols by run_accession (sample ID), study_accession and drugs
-drug_resistance_data <- subset(drug_resistance_data, study_accession == study_accession)
-drug_resistance_data <- drug_resistance_data[, cols]
+dr_data <- subset(metadata, study_accession == study_accession)
+dr_data <- drug_resistance_data$dr_status
+
+sus <- ifelse(dr_data == "Susceptible", 1, -1)
+DR <- ifelse(dr_data == "DR", 1, -1)
+MDR <- ifelse(dr_data == "MDR", 1, -1)
+XDR <- ifelse(dr_data == "XDR", 1, -1)
+
+dr_df <- cbind(metadata$run_accession, sus, DR, MDR, XDR)
+
+# Write dataframe to template
+
+
+write.table(dr_df, file = itol_dr_out_file, 
+            append = T, sep="\t",
+            row.names=F, col.names=F, quote = F)
+
 
 
 

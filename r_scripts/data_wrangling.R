2038cac669fdd4edf0dae313d5722b3e7692f510
Gary Napier <garynapier@Garys-MacBook-Pro-2.local>
2021-02-17 19:28:39 +0000

    initial

---
+++ r_scripts/data_wrangling.R
@@ -0,0 +1,133 @@
+
+
+#!/usr/bin/env Rscript
+
+# data_wrangling.R
+
+# Description
+# Wrangle data for tables and figures
+
+# Input:
+
+# Main steps:
+
+# Output:
+
+
+# Libraries ----
+
+library(plyr)
+library(dplyr)
+library(ggplot2)
+library(reshape2)
+library(janitor)
+
+# Functions ----
+
+heaD <- function(x,...){
+  head(x, ...)
+}
+
+len_str <- function(string){
+  length(unlist(strsplit(string, split = "")))
+}
+
+hs <- function(x, ...){
+  print(head(x, ...))
+  print("---")
+  str(x, ...)
+}
+
+add_commas <- function(x){
+formatted_ns <- attr(x, "core") %>% # extract the tabyl's underlying Ns
+  adorn_totals(c("row", "col")) %>% # to match the data.frame we're appending to
+  dplyr::mutate_if(is.numeric, format, big.mark = ",")
+return(formatted_ns)
+}
+
+to_table <- function(x){
+  x <- x %>% adorn_totals(c("row", "col")) %>%
+    adorn_percentages(c("row")) %>%
+    adorn_pct_formatting(digits = 2)
+  formatted_ns <- attr(x, "core") %>% # extract the tabyl's underlying Ns
+    adorn_totals(c("row", "col")) %>% # to match the data.frame we're appending to
+    dplyr::mutate_if(is.numeric, format, big.mark = ",")
+  x %>% adorn_ns(position = "front", ns = formatted_ns)
+}
+
+# Setup ----
+
+study_accessions <- c("PAKISTAN", "THAILAND")
+id_col <- "wgs_id"
+study_acc_col <- "study_accession_word"
+
+# Paths ----
+
+trans_path <- "~/Documents/transmission/"
+metadata_path <- "~/Documents/metadata/"
+
+setwd(trans_path)
+
+
+# File names ----
+
+metadata_file <- paste0(metadata_path, "tb_data_28_01_2021_clean.csv")
+
+print("FILES:")
+print(c("metadata_file: ", metadata_file))
+
+
+# Read in files ----
+
+metadata <- read.csv(metadata_file, stringsAsFactors = F)
+
+
+# Basic stats ----
+
+metadata <- subset(metadata, study_accession_word %in% study_accessions)
+
+
+# DR summary
+
+# Desired order of DR cols
+dr_vals <- c("Sensitive", "Pre-MDR", "MDR", "Pre-XDR", "XDR", "Other", "NA") 
+
+# Pivot table
+dr <- reshape2::dcast(metadata, study_accession_word ~ dr_status, value.var = id_col, fun.aggregate = length)
+# Re-order the cols
+dr <- dr[, c(study_acc_col, dr_vals)]
+# Re-name
+names(dr) <- c("Study", dr_vals)
+# Add totals and percentages
+to_table(dr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

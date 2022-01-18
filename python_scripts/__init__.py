5cdf1ad362fd212c1d8e6996f410b6f0a6c7b56c
GaryNapier <garynapier83@googlemail.com>
2022-01-18 16:23:38 +0000

    initial

---
+++ python_scripts/__init__.py
@@ -0,0 +1,6 @@
+# from .legs import *
+# from .mouth import *
+
+from .utils import *
+from .tree_functions import *
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+import setuptools
+
+setuptools.setup(
+    name='pipeline',
+    version='0.0.1',
+    packages=['python_scripts']
+)

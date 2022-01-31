f68291382fcf3ece41b590d4f869d0592a22df2d
GaryNapier <garynapier83@googlemail.com>
2022-01-31 17:43:48 +0000

    added tb_data

--- python_scripts/utils.py
+++ python_scripts/utils.py
@@ -7,7 +7,38 @@ import re
 def flat_list(mylist):
     return [item for sublist in mylist for item in sublist]
 
-def invert_dict(d):
+def invert_dict_listed(d):
+    # Dictionary:
+    # x = {'a':['apples'], 'b':['bananas'], 'c':['pears']}
+    # invert_dict_listed(x)
+    # {'apples': ['a'], 'bananas': ['b'], 'pears': ['c']}
+
+    # x = {'a':['apples'], 'b':['bananas'], 'c':['pears', 'grapes']}
+    # invert_dict_listed(x)
+    # invert_dict_listed(x)
+
+    inverse = dict()
+    for key in d:
+        # Go through the list that is saved in the dict:
+        for item in d[key]:
+            # Check if in the inverted dict the key exists
+            if item not in inverse:
+                # If not create a new list
+                inverse[item] = [key]
+            else:
+                inverse[item].append(key)
+    return inverse
+
+def invert_dict_not_listed(d):
+    # Dictionary:
+    # x = {'a':'apples', 'b':'bananas', 'c':'pears'}
+    # invert_dict_not_listed(x)
+    # {'apples': ['a'], 'bananas': ['b'], 'pears': ['c']}
+
+    # Convert to list in case it isn't
+    for key in d:
+        d[key] = [d[key]]
+
     inverse = dict()
     for key in d:
         # Go through the list that is saved in the dict:
@@ -114,7 +145,6 @@ def reformat_mutations(x):
     else:
         return None
 
-
 def resolve_lineages(data):
     # Examples:
 
@@ -143,24 +173,4 @@ def resolve_lineages(data):
         if not improved:
             break
     return data
-
-
-# x = {
-#     'lineage1.2.1.2.1': 1,
-#     'lineage4.1.2.1': 1,
-#     'lineage4.1.1.3': 1,
-#     'lineage4.1.1': 1,
-#     'lineage4.2.1': 1,
-#     'lineage4.2.1.1': 1
-#     }
-
-# data = dict(x)
-
-# # sorted(data,reverse= True,key=lambda x : len(x))
-# for child in sorted(data,reverse= True,key=lambda x : len(x)):
-#     print('CHILD')
-#     print(child)
-#     parent = ".".join(child.split(".")[:-1])
-#     print('PARENT')
-#     print(parent)
 
